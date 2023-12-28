6c918da1132404babdf1656528915a784f8ba4c6
jeraj636 <58883210+jeraj636@users.noreply.github.com>
2023-12-28 16:34:53 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include "CelicniAvtomat.h"
+
+int main()
+{
+    int x;
+    while (true)
+    {
+        std::cin >> x;
+        CelicniAvtomat m(370, 100, time(NULL), 53);
+        for (int i = 0; i < 500; i++)
+            m.Sim();
+        std::cout << m << "\n"
+                  << m.Procent() << "\n";
+    }
+}
